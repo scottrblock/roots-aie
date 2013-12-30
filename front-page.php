af9b15a12ad9bd759e735ab4b8e9304309c199a9
Scott Block <scottrblock@gmail.com>
2013-12-30 05:06:45 -0500

    front page and footer

---
+++ front-page.php
@@ -0,0 +1,201 @@
+<?php get_template_part('templates/page', 'header'); ?>
+
+<div id="fearlessEducation">
+  <div class="container">
+    <div class="callout col-md-6 col-md-offset-6">
+      <div class="title">
+        <h1>
+          Fearless Education
+        </h1>
+      </div>
+      <div class="text">
+        <p>
+          We do stuff that involves stuff that helps stuff do stuff. We do stuff that involves
+          stuff that helps stuff do stuff. We do stuff that involves stuff that helps stuff
+          do stuff. We do stuff that involves stuff that helps stuff do stuff. We do stuff
+          that involves stuff that helps stuff do stuff.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="theLatest">
+  <div class="row">
+    <div class="title">
+      <h3>
+        The Latest
+      </h3>
+    </div>
+    <div class="toggle-events">
+      <p>
+        All events
+        <span class="separater">
+          /
+        </span>
+        All news
+      </p>
+    </div>
+  </div>
+  <div class="container latest-content">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="event">
+          <p class="date">
+            Jackson Geller is cool
+          </p>
+          <p class="title">
+            Innovation Friday
+          </p>
+          <p class="desc">
+            Do stuff that is cool and stuff
+          </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="event">
+          <p class="date">
+            Jackson Geller is cool
+          </p>
+          <p class="title">
+            Innovation Friday
+          </p>
+          <p class="desc">
+            Do stuff that is cool and stuff
+          </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="event">
+          <p class="date">
+            Jackson Geller is cool
+          </p>
+          <p class="title">
+            Innovation Friday
+          </p>
+          <p class="desc">
+            Do stuff that is cool and stuff
+          </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="event">
+          <p class="date">
+            Jackson Geller is cool
+          </p>
+          <p class="title">
+            Innovation Friday
+          </p>
+          <p class="desc">
+            Do stuff that is cool and stuff
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="row latest-content">
+      <div class="col-md-6">
+        <div class="note">
+          <div class="title">
+            <h5>
+              Actually kitsch fanny pack tattooed, Etsy bespoke
+            </h5>
+          </div>
+          <div class="content">
+            <p>
+              is simply dummy text of the printing and typesetting industry Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500 when an unknown printer took
+              a galley of type and scrambled it to make unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing Lorem Ipsum
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="note">
+          <div class="title">
+            <h5>
+              Actually kitsch fanny pack tattooed, Etsy bespoke
+            </h5>
+          </div>
+          <div class="content">
+            <p>
+              is simply dummy text of the printing and typesetting industry Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500 when an unknown printer took
+              a galley of type and scrambled it to make unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing Lorem Ipsum
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row latest-content">
+      <div class="col-md-6">
+        <div class="note">
+          <div class="title">
+            <h5>
+              Actually kitsch fanny pack tattooed, Etsy bespoke
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="note">
+          <div class="title">
+            <h5>
+              Actually kitsch fanny pack tattooed, Etsy bespoke
+            </h5>
+          </div>
+          <div class="content">
+            <p>
+              is simply dummy text of the printing and typesetting industry Lorem Ipsum has been
+              the industry's standard dummy text ever since the 1500 when an unknown printer took
+              a galley of type and scrambled it to make unchanged. It was popularised in the 1960s
+              with the release of Letraset sheets containing Lorem Ipsum
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="quote1">
+  <div class="quote-text">
+  </div>
+</div>
+<div id="resourceMap">
+  <div class="title">
+    <div class="container">
+    </div>
+  </div>
+  <div class="map-instructions">
+  </div>
+  <div class="container">
+    <div class="col-md-2 map-sidebar">
+      <div class="title">
+      </div>
+      <div class="toggles">
+      </div>
+      <div class="title">
+      </div>
+      <div class="toggles">
+      </div>
+    </div>
+  </div>
+</div>
+<div id="quote2">
+  <div class="quote-text">
+  </div>
+</div>
+<div id="welcomeLetter">
+  <div class="title">
+    <div class="container">
+    </div>
+  </div>
+  <div class="container">
+    <div class="picture">
+    </div>
+    <div class="letter-text">
+    </div>
+  </div>
+</div>
+
+<?php get_template_part('templates/content', 'page'); ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,35 @@
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<footer class="content-info container" role="contentinfo" id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 email">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-1 footer-col">
+      </div>
+      <div class="col-md-1 col-md-offset-1 footer-col">
+      </div>
+      <div class="col-md-1 col-md-offset-1 footer-col">
+      </div>
+      <div class="col-md-1 col-md-offset-1 footer-col">
+      </div>
+      <div class="col-md-3 col-md-offset-8 footer-umd">
+      </div>
+    </div>
+    <div class="row">
+      <div class="legal">
+      </div>
+      <div class="contacts">
+      </div>
     </div>
   </div>
 </footer>
