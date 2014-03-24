677bb30767329d049d78805db2d0129317386a2d
Scott Block <scottrblock@gmail.com>
2014-03-24 16:52:18 -0400

    header in grid

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '47fd90ce722540d9b4b1bc89944f6b99');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '712516e9b44ae8bc57098a2b57357289');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
