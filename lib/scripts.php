dbda7d6424cdcf9747a24ac4d60688688f0de577
jacksongeller <jacksongeller@gmail.com>
2014-04-02 15:17:38 -0400

    quickie

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd83e6db4d0d1fb1888a13e444b104d9e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dee91a9c67ed58f7d2f848892728289b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
